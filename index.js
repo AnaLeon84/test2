e636df62b54bb12a2d34f434d2f1019f67a52d5d
AnaLeon84 <ana.cableon84@gmail.com>
2022-11-12 16:16:34 +0100

    oblog v2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//test
+const express = require('express');
+require("dotenv").config();
+const articles = require('./data/articles.json');
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, ()=> {
+    console.log(`App listening at https://localhost:${PORT}`);
+})
+
+const app = express()
